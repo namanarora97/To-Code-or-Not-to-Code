0851ef9c30e37dc4f42381168d2d6c337896a88b
Naman Arora <namanarora@cmu.edu>
2022-04-28 15:38:04 -0400

    added docstring to MyBinarizer

--- MyBinarizer.py
+++ MyBinarizer.py
@@ -2,13 +2,22 @@ from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.base import BaseEstimator, TransformerMixin
 import pandas as pd
 
+# This class is used to binarize a multi-label column
+# It can be understood as a multi-label one-hot encoder
+
 
 class MyBinarizer(BaseEstimator, TransformerMixin):
     def __init__(self):
+        """
+        Set up the class
+        """
         print(">>>> Binarizer Initialized")
         self.mlb_list = []
 
     def fit(self, X, y=None):
+        """
+        Fit the binarizer on all the features in the dataframe
+        """
         print(">>>> Fit called")
         for column in list(X.columns):
             mlb = MultiLabelBinarizer()
@@ -17,6 +26,9 @@ class MyBinarizer(BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, X, y=None):
+        """
+        Return the transformed dataframe
+        """
         print(">>>> Transform called")
         X_ = pd.DataFrame()
         for item in self.mlb_list:
